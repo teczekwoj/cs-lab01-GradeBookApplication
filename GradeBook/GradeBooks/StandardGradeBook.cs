c70d21499cc32bcd175ef68da6b6b33083ec5999
teczekwoj <wlkr2000@gmail.com>
2022-03-16 20:45:26 +0100

    Creating the StandardGradeBook class

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook //klasa dziedzicząca
+    {
+        public StandardGradeBook(string name) : base(name) //konstruktor
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
+    }
+}
